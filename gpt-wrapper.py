7d609da27e66ae18905f89895ba9b3103f93271d
Mardiat-Iman <137158417+Mardiat-Iman@users.noreply.github.com>
2025-01-13 21:54:35 +0000

    First completion

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,1 +1,16 @@
 #file to serve as GPT connection
+import streamlit as st
+
+
+api_key = st.secrets["api_key"]
+#Developer quickstart-from homepge of openAI
+from openai import OpenAI
+client = OpenAI(api_key = "api_key")
+completion = client.chat.completions.create(
+    model = "gpt-4o",
+    store=True, #remove this if it doesn't work
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+print(completion.choices[0].message.content)
