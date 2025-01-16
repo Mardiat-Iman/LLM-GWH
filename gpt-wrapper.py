bb5694fa6667d0a1b53fac6522a36fe840e6b6a6
Mardiat-Iman <137158417+Mardiat-Iman@users.noreply.github.com>
2025-01-16 12:09:58 +0000

    functions for text and image generation-gpt

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,16 +1,36 @@
 #file to serve as GPT connection
 import streamlit as st
-
+from openai import OpenAI
+import requests
 
 api_key = st.secrets["api_key"]
 #Developer quickstart-from homepge of openAI
-from openai import OpenAI
+
 client = OpenAI(api_key = "api_key")
-completion = client.chat.completions.create(
-    model = "gpt-4o",
-    store=True, #remove this if it doesn't work
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
-)
-print(completion.choices[0].message.content)
+
+#function to generate text
+def generate_text(prompt):
+    completion = client.chat.completions.create(
+         model = "gpt-4o",
+         store=True, #remove this if it doesn't work
+         messages=[
+        {"role": "user", "content": prompt}
+        ]
+    ) 
+    return completion.choices[0].message.content
+
+#function to generate image from prompts using DALL-E
+def generate_image(prompt):
+    response = client.images.generate(
+         model = "dall-e-3",
+           prompt = "a white siamese cat",
+           size="1024x1024",
+           quality="standard",
+           n=1,
+    )
+    image_url = response.data[0].url
+
+    image_response = requests.get(image_url)
+
+    with open("cat.png", 'wb') as file:
+        file.write(image_response.content)
